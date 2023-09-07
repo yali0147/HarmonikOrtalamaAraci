798ccc589f9656d2c75f9a9912b23155e47ee4a8
yali <yusufali0147@hotmail.com>
2023-09-07 10:59:51 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Harmonik Ortalama ");
+
+        Scanner imp=new Scanner(System.in);
+        System.out.print("Harmonik Ortalaması alınacak 'n' sayısını girin : ");
+        int n =imp.nextInt();
+       double result=0;
+
+        for (double i=1;i<=n;i++) {
+           result+= (1 / i);
+        }
+        System.out.println("'"+n+"'" +"sayısının Harmonik Ortalaması : " + result);
     }
 }
